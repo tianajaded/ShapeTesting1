d6e2b46a974dc5b9da5d44825ed05c5952ce967e
tiana <tiananoll@gmail.com>
2022-04-03 20:07:15 -0700

    UpdatedCircleTest

--- tests/CircleTest.java
+++ tests/CircleTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CircleTest {
@@ -9,16 +10,25 @@ public class CircleTest {
     // * for testing. Yes, this routine would be problematic
     // * if the constructor tests fail.
     // */
-    // @Before
-    // public void setup() {
-    // circle1 = new CptS132Circle(1);
-    // circle2 = new CptS132Circle(5);
-    // circle3 = new CptS132Circle(0);
-    // }
+    public CptS132Circle unitUnderTest;
+    public int diameter = 5;
+
+    @Before
+    public void setup() {
+        unitUnderTest = new CptS132Circle(diameter);
+
+    }
 
     @Test
-    public void sanityTest() {
-        assertEquals(1 + 1, 2);
+    public void test_getDiameter() {
+        assertEquals(diameter, unitUnderTest.getDiameter());
+
+    }
+
+    @Test
+    public void test_getArea_upToTwoDecPrecision() {
+        double area = unitUnderTest.getArea();
+        assertEquals(19.63, area, 0.005);
     }
 
     // variables for test cases
@@ -32,85 +42,22 @@ public class CircleTest {
     // new CptS132Circle(0);
     // }
 
-    // /**
-    // * Error case constructor test. <br/>
-    // * Parameter value: -2, 5
-    // */
-    // @Test(expected = IllegalArgumentException.class)
-    // public void testConstructor_2() {
-    // new CptS132Circle(-2);
-    // }
+    /**
+     * Error case constructor test. <br/>
+     * Parameter value: -2, 5
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor_2() {
+        new CptS132Circle(-2);
+    }
 
-    // /**
-    // * Error case constructor test. <br/>
-    // * Parameter value: 2, -5
-    // */
-    // @Test(expected = IllegalArgumentException.class)
-    // public void testConstructor_5() {
-    // new CptS132Circle(-5);
-    // }
-
-    // /**
-    // * Error case constructor test. <br/>
-    // * Parameter value: 20000, 5
-    // */
-    // @Test(expected = IllegalArgumentException.class)
-    // public void testConstructor20000() {
-    // new CptS132Circle(20000);
-    // }
-
-    // /**
-    // * Error case constructor test. <br/>
-    // * Parameter value: 2, 50000
-    // */
-    // @Test(expected = IllegalArgumentException.class)
-    // public void testConstructor50000() {
-    // new CptS132Circle(50000);
-    // }
-
-    // /**
-    // * Testing getWidth, case 2x1
-    // */
-    // @Test
-    // public void testDiameter() {
-    // double expected = 1;
-    // assertEquals("Width 2x1", expected, rect2x1.getWidth(), 1e-9);
-    // }
-
-    // /**
-    // * Testing getWidth, case 5x5
-    // */
-    // @Test
-    // public void testDiameter2x3() {
-    // double expected = 5;
-    // assertEquals("Width 5x5", expected, rect5x5.getWidth(), 1e-8);
-    // }
-
-    // /**
-    // * Testing getArea, case 2x1
-    // */
-    // @Test
-    // public void testArea2x1() {
-    // double expected = 2;
-    // assertEquals("Area 2x1", expected, rect2x1.getArea(), 1e-9);
-    // }
-
-    // /**
-    // * Testing getArea, case 5x5
-    // */
-    // @Test
-    // public void testArea5x5() {
-    // double expected = 25;
-    // assertEquals("Area 5x5", expected, rect5x5.getArea(), 1e-8);
-    // }
-
-    // /**
-    // * Testing getArea, case 0x3
-    // */
-    // @Test
-    // public void testArea0x3() {
-    // double expected = 0;
-    // assertEquals("Area 0x3", expected, rect0x3.getArea(), 1e-9);
-    // }
+    /**
+     * Error case constructor test. <br/>
+     * Parameter value: 2, -5
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructor_5() {
+        new CptS132Circle(-5);
+    }
 
 }
